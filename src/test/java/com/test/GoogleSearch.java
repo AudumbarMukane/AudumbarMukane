993e004a93ef2de109d4c9105eee406c91333146
Praveen Dharman <praveen.dharman@testsolutions.de>
2025-11-06 13:54:23 +0530

    My first commit

---
+++ src/test/java/com/test/GoogleSearch.java
@@ -0,0 +1,45 @@
+package com.test;
+
+import com.microsoft.playwright.Locator;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class GoogleSearch {
+ 
+	public static void main(String[] args) {
+		
+	    // Create a Playwright instance
+        try (Playwright playwright = Playwright.create()) {
+            // Launch a Chromium browser (set headless=false to see the browser)
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            
+            // Create a new browser context and page
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+
+            // Navigate to Google
+            page.navigate("https://www.flipkart.com/");
+
+            page.waitForLoadState();
+            // Optional: Print the page title
+            System.out.println("Page title: " + page.title());
+
+            // Wait for a few seconds so you can see the page
+            //page.waitForTimeout(5000);
+         
+         // Locate the element
+            Locator element = page.locator("//span[@role='button']");
+
+            
+
+            // Close everything
+            browser.close();
+        }
+    }
+
+	}
+
